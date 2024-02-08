c7561e0c365ec78cd3bd8211fabf14582baef6ef
Ian-Ammerman <ianammerman34@gmail.com>
2024-02-08 16:25:44 -0500

    Update Github

---
+++ OpenFAST_Driver.m
@@ -0,0 +1,73 @@
+% Basic OpenFAST driver for new turbine testing models
+clear all; close all; clc;
+
+%% ---------- USER INPUTS ---------- %%
+home_dir = 'C:\\Umaine Google Sync\\GitHub\\Robust_Test_Turbine';
+
+% Model to test (must match folder name in 'Models' folder)
+model = 'FOCAL_C1';
+fst_name = 'Turbine_asBuilt';
+
+% Simulation folder for outputs
+sim_folder = 'Steady_12ms';
+
+% OpenFAST version (name of corresponding folder in 'bin')
+version = 'FC1';
+bin_name = 'openfast_x64_dev.exe';
+
+%% ---------- END USER INPUTS ---------- %%
+
+
+
+
+
+
+
+%% ---------- BEGIN CODE ---------- %%
+% Ensure starting from home dir
+if ~strcmp(pwd,home_dir)
+    cd(home_dir)
+end
+
+% Directory Definitions (relative to Robust_Test_Turbine repo)
+bin_dir = sprintf('bin/%s/%s',version,bin_name);
+model_dir = sprintf('Models/%s/%s.fst',model,fst_name);
+sim_dir = sprintf('Simulations/%s',sim_folder);
+
+% Prepare simulation directory
+cd('Simulations')
+
+if ~exist(sim_folder,'dir')
+    mkdir(sim_folder)
+end
+
+cd('..')
+
+% Run OpenFAST Simulation
+name = sprintf('"%s/%s" "%s/%s"', home_dir, bin_dir, home_dir, model_dir);
+[status,results] = system(name,'-echo');
+
+% Relocate output files to simulation directory
+disp('---------- Relocating Output Files -----------')
+cd(sim_dir)
+
+filetypes = {'.out','.ech','.sum','.outb'};
+for i = 1:length(filetypes)
+    try
+        movefile(sprintf('../../Models/%s/*%s',model,filetypes{i}));
+        fprintf('Output files of type %s relocated. \n',filetypes{i});
+    catch
+        fprintf('No files of type %s detected. \n',filetypes{i});
+    end
+end
+
+% Process output files & format as structure (assumes .out format)
+[sim_results,units] = readFastTabular(sprintf('%s.out',fst_name));
+
+% Save results structure named after model used
+save_name = sprintf('%s_FAST_Results.mat',fst_name);
+save(save_name,'sim_results');
+
+% Relocate
+fclose('all');
+
---
+++ OpenFAST_Plotter.m
@@ -0,0 +1,58 @@
+% Generic plotting script
+clear all; close all; clc;
+
+%% ---------- USER INPUTS ---------- %%
+
+% Models to plot
+plot_models = {'Turbine_asBuilt'};
+
+% Channels to plot
+plot_channels = {'GenSpeed'};
+
+% Time range to plot
+t_range = [0,100];
+
+%% ---------- END USER INPUTS ---------- %%
+
+
+
+
+%% ---------- BEGIN CODE ---------- %%
+
+% Load in results
+for model = 1:length(plot_models)
+
+    % Results to load
+    current_results = sprintf('%s_FAST_Results.mat',plot_models{model});
+    
+    % Load in results
+    load(current_results);
+
+    % Store in larger structure
+    results.(plot_models{model}) = sim_results;
+
+    % Clear out individual sim results
+    clear sim_results
+
+end
+
+% Loop over channels to plot
+for f = 1:length(plot_channels)
+    
+    % Generate figure
+    figure
+    gca; hold on; box on;
+
+    % Loop over models & plot
+    for m = 1:length(plot_models)
+        plot(results.(plot_models{m}).Time,...
+             results.(plot_models{m}).(plot_channels{m}),...
+             'DisplayName',plot_models{m});
+    end
+
+    % Add legend & labels
+    title(sprintf('%s',plot_channels{f}))
+    xlabel('Time [s]')
+    legend('Interpreter','none')
+
+end
